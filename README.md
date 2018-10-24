a9cef8c782364872136a020d11bc5004712186d0
wzg <826679571@qq.com>
2018-10-24 22:12:31 +0800

    迁入

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PollyDemo
-Polly demo
+Polly 支持的机制
+* 1.Retry 重试机制 （多次、永远、按照时间间隔重试等）
+* 2.Circuit-breaker 熔断
---
+++ Retry/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Polly;
+
+namespace PollyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Handle Exception
+            Policy
+            .Handle<Exception>();
+
+            //2.重试3次
+            Policy
+            .Handle<Exception>()
+            .Retry(3);
+
+            //3.按照不同时间间隔重试
+            Policy
+            .Handle<Exception>()
+            .WaitAndRetry(new [] {
+                TimeSpan.FromSeconds(1),
+                TimeSpan.FromSeconds(2),
+                TimeSpan.FromSeconds(3)}
+            );
+            //4.永远重试
+            Policy
+          .Handle<Exception>()//这里可以根据不同的异常进行过滤，确认是否要执行后面的操作
+          .RetryForever(exception =>
+          {
+              // do something       
+          });
+        }
+    }
+}
