b2d9e4873b22a68d3ef7f825004957e073aa0c86
wangzhanguo <wangzhanguovip@outlook.com>
2018-11-05 11:18:11 +0800

    polly demo

--- Polly/README.md
+++ Polly/README.md
@@ -2,3 +2,8 @@
 Polly 支持的机制
 * 1.Retry 重试机制 （多次、永远、按照时间间隔重试等）
 * 2.Circuit-breaker 熔断
+* 3.timeout 超时
+* 4.Bulkhead Isolation 隔离（下游得系统异常可能会影响上游得系统）
+* 5.Cache 缓存
+* 6.Fallback 回路（对于预知异常得处理）
+* 7.PolicyWrap 策略（对于不同异常适配得异常处理）
--- Polly/Retry/Program.cs
+++ Polly/Retry/Program.cs
@@ -1,36 +1,101 @@
 ﻿using System;
+using System.Threading;
 using Polly;
 
 namespace PollyDemo
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //1.Handle Exception
-            Policy
-            .Handle<Exception>();
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Retry();
+			CircuitBreaker();
+		}
 
-            //2.重试3次
-            Policy
-            .Handle<Exception>()
-            .Retry(3);
+		static void Retry()
+		{
+			//1.Handle Exception
+			var p = Policy
+			.Handle<Exception>()
+			.Retry();
 
-            //3.按照不同时间间隔重试
-            Policy
-            .Handle<Exception>()
-            .WaitAndRetry(new [] {
-                TimeSpan.FromSeconds(1),
-                TimeSpan.FromSeconds(2),
-                TimeSpan.FromSeconds(3)}
-            );
-            //4.永远重试
-            Policy
-          .Handle<Exception>()//这里可以根据不同的异常进行过滤，确认是否要执行后面的操作
-          .RetryForever(exception =>
-          {
-              // do something       
-          });
-        }
-    }
+			p.Execute(() =>
+			{
+				Console.WriteLine("执行一次");
+				throw new Exception("test");
+			});
+
+			//2.重试3次
+			p = Policy
+			.Handle<Exception>()
+			.Retry(3);
+
+			var context = new Context();
+			context.Add("index", 0);
+			p.Execute((c) =>
+			{
+				Console.WriteLine("执行" + c["index"] + "次");
+				c["index"] = Convert.ToInt32(c["index"]) + 1;
+				throw new Exception("test");
+			}, context);
+
+			//3.按照不同时间间隔重试
+			p = Policy
+			.Handle<Exception>()
+			.WaitAndRetry(new[] {
+				TimeSpan.FromSeconds(1),
+				TimeSpan.FromSeconds(2),
+				TimeSpan.FromSeconds(3)}
+			);
+
+			p.Execute(() =>
+			{
+				Console.WriteLine("执行一次" + DateTime.Now);
+				throw new Exception("test");
+			});
+
+			//4.永远重试
+			p = Policy
+		  .Handle<Exception>()//这里可以根据不同的异常进行过滤，确认是否要执行后面的操作
+		  .RetryForever(exception =>
+		  {
+			  // do something       
+		  });
+			p.Execute(() =>
+			{
+				Console.WriteLine("执行一次" + DateTime.Now);
+				throw new Exception("test");
+			});
+		}
+
+		static void CircuitBreaker()
+		{
+			//一分钟内连续2次 异常则熔断
+			var p = Policy
+					.Handle<Exception>()
+					.CircuitBreaker(8, TimeSpan.FromSeconds(10));
+
+			while (true)
+			{
+				try
+				{
+					p.Execute(() =>
+					{
+						Console.WriteLine("Job Start");
+						throw new Exception("Special error occured");
+						Console.WriteLine("Job End");
+					});
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine("There's one unhandled exception : " + ex.Message);
+				}
+
+				Thread.Sleep(500);
+			}
+
+		}
+
+
+	}
 }
