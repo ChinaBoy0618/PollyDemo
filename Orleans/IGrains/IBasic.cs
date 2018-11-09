81fb9993139af3df58d4c08b6fee6a201586b12a
wangzhanguo <wangzhanguovip@outlook.com>
2018-11-09 17:42:44 +0800

    orleans

---
+++ Orleans/IGrains/IBasic.cs
@@ -0,0 +1,13 @@
+﻿using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGrains
+{
+	public interface IBasic:IGrainWithIntegerKey
+	{
+		Task<string> SayHello(string hellostr);
+	}
+}
